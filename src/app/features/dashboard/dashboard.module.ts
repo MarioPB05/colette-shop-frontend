d493a1cfec0cba780c8c4b23f53ee99f69f38ac7
DavidClase <dzamoramartinez@safareyes.es>
2025-01-29 21:06:08 +0100

    ♻️ Add brawler-page component

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {DashboardRoutesModule} from '@dashboard/dashboard.routes';
 import {DashboardPageComponent} from '@dashboard/pages/dashboard-page.component';
 import {StatsPageComponent} from '@dashboard/pages/stats-page/stats-page.component';
+import {BrawlersPageComponent} from '@dashboard/pages/brawlers-page/brawlers-page.component';
 
 @NgModule({
   declarations: [],
   imports: [
     DashboardPageComponent,
     StatsPageComponent,
+    BrawlersPageComponent,
     DashboardRoutesModule,
   ]
 })
