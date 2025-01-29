5131a716e770ddff839aa918fdd8bfd5a0d3a124
david <dperezfernandez@safareyes.es>
2025-01-29 21:11:53 +0100

    ✨ Created brawl header component

---
+++ src/app/shared/components/brawl-header/brawl-header.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-brawl-header',
+  imports: [],
+  templateUrl: './brawl-header.component.html',
+  standalone: true,
+  styleUrl: './../../brawl_styles.scss',
+})
+export class BrawlHeaderComponent {
+
+}
