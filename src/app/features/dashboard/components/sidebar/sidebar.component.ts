5100a76200b8316192a0a69573db8f24809aac45
Mario <mperdiguerobarrera@safareyes.es>
2025-01-29 20:36:28 +0100

    ♻️ Add menu items

--- src/app/features/dashboard/components/sidebar/sidebar.component.ts
+++ src/app/features/dashboard/components/sidebar/sidebar.component.ts
@@ -1,62 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import {MenuModule} from 'primeng/menu';
 import {BadgeModule} from 'primeng/badge';
 import {RippleModule} from 'primeng/ripple';
 import {AvatarModule} from 'primeng/avatar';
 import {MenuItem} from 'primeng/api';
 import {NgIf} from '@angular/common';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [MenuModule, BadgeModule, RippleModule, AvatarModule, NgIf],
+  imports: [MenuModule, BadgeModule, RippleModule, AvatarModule, NgIf, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styles: ``
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  @HostBinding('class') class = 'h-screen flex py-10';
   items: MenuItem[] | undefined;
 
   ngOnInit() {
     this.items = [
       {
-        separator: true
+        label: 'Estadísticas',
+        icon: 'pi pi-fw pi-chart-bar',
+        route: '/dashboard/stats'
       },
       {
-        label: 'Documents',
-        items: [
-          {
-            label: 'New',
-            icon: 'pi pi-plus',
-            shortcut: '⌘+N'
-          },
-          {
-            label: 'Search',
-            icon: 'pi pi-search',
-            shortcut: '⌘+S'
-          }
-        ]
+        label: 'Usuarios',
+        icon: 'pi pi-fw pi-users',
+        route: '/dashboard/users'
       },
       {
-        label: 'Profile',
-        items: [
-          {
-            label: 'Settings',
-            icon: 'pi pi-cog',
-            shortcut: '⌘+O'
-          },
-          {
-            label: 'Messages',
-            icon: 'pi pi-inbox',
-            badge: '2'
-          },
-          {
-            label: 'Logout',
-            icon: 'pi pi-sign-out',
-            shortcut: '⌘+Q'
-          }
-        ]
+        label: 'Pedidos',
+        icon: 'pi pi-fw pi-shopping-bag',
+        route: '/dashboard/orders'
       },
       {
-        separator: true
+        label: 'Cajas',
+        icon: 'pi pi-fw pi-box',
+        route: '/dashboard/boxes'
+      },
+      {
+        label: 'Brawlers',
+        icon: 'pi pi-fw pi-star',
+        route: '/dashboard/brawlers'
       }
     ];
   }
