f3a2cd1a6069aa6c29e1b7a102b6c45fc4724584
Mario <mperdiguerobarrera@safareyes.es>
2025-01-29 20:37:45 +0100

    ♻️ Import components

--- src/app/features/dashboard/pages/dashboard-page.component.ts
+++ src/app/features/dashboard/pages/dashboard-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {SidebarComponent} from '@dashboard/components/sidebar/sidebar.component';
+import {Card} from "primeng/card";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [
-    RouterOutlet,
-    SidebarComponent
-  ],
+    imports: [
+        RouterOutlet,
+        SidebarComponent,
+        Card
+    ],
   templateUrl: './dashboard-page.component.html',
   styles: ``
 })
