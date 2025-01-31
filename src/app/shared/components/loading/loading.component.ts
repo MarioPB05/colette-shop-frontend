63e6803b92ce60483f59beb34c843a5c91e2ce5e
Mario <mperdiguerobarrera@safareyes.es>
2025-01-31 17:15:01 +0100

    ✨ Create loading logic

---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,44 @@
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {Observable, tap} from 'rxjs';
+import {RouteConfigLoadEnd, RouteConfigLoadStart, Router} from '@angular/router';
+import {LoadingService} from '@core/services/loading.service';
+import {AsyncPipe} from '@angular/common';
+
+@Component({
+  selector: 'app-loading',
+  imports: [
+    AsyncPipe
+  ],
+  templateUrl: './loading.component.html'
+})
+export class LoadingComponent implements OnInit {
+  @HostBinding('class') class = 'absolute z-50 bg-brawl-blue overflow-hidden';
+
+  loading$: Observable<boolean>;
+
+  @Input()
+  detectRouteTransitions = false;
+
+  constructor(
+    private loadingService: LoadingService,
+    private router: Router) {
+    this.loading$ = this.loadingService.loading$;
+  }
+
+  ngOnInit() {
+    if (this.detectRouteTransitions) {
+      this.router.events
+        .pipe(
+          tap((event) => {
+            if (event instanceof RouteConfigLoadStart) {
+              this.loadingService.loadingOn();
+            } else if (event instanceof RouteConfigLoadEnd) {
+              this.loadingService.loadingOff();
+            }
+          })
+        )
+        .subscribe();
+    }
+  }
+
+}
