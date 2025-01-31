84fb96629d138f0cf274a6606991faba65358a1d
DavidClase <dzamoramartinez@safareyes.es>
2025-01-31 20:43:26 +0100

    ♻️ Add user and order pages

--- src/app/features/dashboard/dashboard.routes.ts
+++ src/app/features/dashboard/dashboard.routes.ts
@@ -3,6 +3,8 @@ import {NgModule} from '@angular/core';
 import {DashboardPageComponent} from '@dashboard/pages/dashboard-page.component';
 import {StatsPageComponent} from '@dashboard/pages/stats-page/stats-page.component';
 import {BrawlersPageComponent} from '@dashboard/pages/brawlers-page/brawlers-page.component';
+import {UserPageComponent} from '@dashboard/pages/user-page/user-page.component';
+import {OrderPageComponent} from '@dashboard/pages/order-page/order-page.component';
 
 const routes: Routes = [
   {
@@ -16,6 +18,14 @@ const routes: Routes = [
       {
         path: 'brawlers',
         component: BrawlersPageComponent
+      },
+      {
+        path: 'user',
+        component: UserPageComponent
+      },
+      {
+        path: 'order',
+        component: OrderPageComponent
       },
       {
         path: '',
