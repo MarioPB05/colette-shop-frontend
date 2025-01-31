f2bc7c895a34cc847252bc15e2d8be4ce07a2f7b
david <dperezfernandez@safareyes.es>
2025-01-31 17:13:05 +0100

    📱 Added a custom break point for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,5 +30,13 @@ module.exports = {
       }
     },
     plugins: [],
+    screens: {
+      'xs': '500px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   }
 }
