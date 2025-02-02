99d38ba3ccadb609d1d802323b5e5a336600d495
Mario <mperdiguerobarrera@safareyes.es>
2025-02-02 14:09:29 +0100

    ♻️ Rename pages folder name

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -3,8 +3,8 @@ import {DashboardRoutesModule} from '@dashboard/dashboard.routes';
 import {DashboardPageComponent} from '@dashboard/pages/dashboard-page.component';
 import {StatsPageComponent} from '@dashboard/pages/stats-page/stats-page.component';
 import {BrawlersPageComponent} from '@dashboard/pages/brawlers-page/brawlers-page.component';
-import {UserPageComponent} from '@dashboard/pages/user-page/user-page.component';
-import {OrderPageComponent} from '@dashboard/pages/order-page/order-page.component';
+import {UserPageComponent} from '@dashboard/pages/users-page/user-page.component';
+import {OrderPageComponent} from '@dashboard/pages/orders-page/order-page.component';
 
 @NgModule({
   declarations: [],
