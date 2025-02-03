c93340679625824bc570637126c94a2a60d3f978
david <dperezfernandez@safareyes.es>
2025-02-03 14:00:20 +0100

    ✨ Add slider sync between its inputs

--- src/app/features/catalog/pages/catalog-page/catalog-page.component.ts
+++ src/app/features/catalog/pages/catalog-page/catalog-page.component.ts
@@ -1,31 +1,63 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {BrawlHeaderComponent} from '@shared/components/brawl-header/brawl-header.component';
+import {BoxBuyCardComponent} from '@features/catalog/components/box-buy-card/box-buy-card.component';
 import {Slider} from 'primeng/slider';
 import {FormsModule} from '@angular/forms';
 import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-shop-page',
-  imports: [BrawlHeaderComponent, Slider, FormsModule, InputNumberModule],
+  imports: [BrawlHeaderComponent, Slider, FormsModule, InputNumberModule, BoxBuyCardComponent],
   templateUrl: './catalog-page.component.html',
   styleUrl: './../../../../shared/brawl_styles.scss',
   standalone: true
 })
 export class CatalogPageComponent {
   rangeValues: number[] = [20, 30];
-  minimumPrice: number = 20;
-  maximumPrice: number = 30;
 
-  onSliderChange(event: any) {
-    this.minimumPrice = event.values[0];
-    this.maximumPrice = event.values[1];
+  extractOnlyNumbers(str: string): number {
+    const cleanedStr = str.replace(/(?<=\d)[.,](?=\d{3}(?:[.,]|$))/g, "");
+    const normalizedStr = cleanedStr.replace(/[,\.](?=\d{1,2}$)/, ".");
+    const num = parseFloat(normalizedStr);
+    return isNaN(num) ? 0 : num;
   }
 
-  minPriceChange(event: any) {
-    this.minimumPrice = event.target.value;
+  swapSliderValues() {
+    const temp = this.rangeValues[0];
+    this.rangeValues[0] = this.rangeValues[1];
+    this.rangeValues[1] = temp;
+    this.rangeValues = [...this.rangeValues];
   }
 
-  maxPriceChange(event: any) {
-    this.maximumPrice = event.target.value;
+  minPriceChange($event: KeyboardEvent) {
+    const value = this.extractOnlyNumbers(($event.target as HTMLInputElement).value);
+
+    if (value > 50) {
+      this.rangeValues[0] = 50;
+    }else {
+      this.rangeValues[0] = value;
+    }
+
+    if (this.rangeValues[0] > this.rangeValues[1]) {
+      this.swapSliderValues();
+    }
+
+    this.rangeValues = [...this.rangeValues];
+  }
+
+  maxPriceChange($event: KeyboardEvent) {
+    const value = this.extractOnlyNumbers(($event.target as HTMLInputElement).value);
+
+    if (value > 50) {
+      this.rangeValues[1] = 50;
+    }else {
+      this.rangeValues[1] = value;
+    }
+
+    if (this.rangeValues[0] > this.rangeValues[1]) {
+      this.swapSliderValues();
+    }
+
+    this.rangeValues = [...this.rangeValues];
   }
 }
