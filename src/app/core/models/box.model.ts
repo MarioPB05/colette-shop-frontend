09dfdaf70a46d0d23e46d37bdbe54e4d8a17639b
david <dperezfernandez@safareyes.es>
2025-02-03 14:02:18 +0100

    ✨ Create interface for the bos in the shop

---
+++ src/app/core/models/box.model.ts
@@ -0,0 +1,11 @@
+
+export interface BoxShopResponse {
+  id: number;
+  name: string;
+  price: number;
+  type: string;
+  boxesLeft: number;
+  favoriteBrawlersInBox: number;
+  pinned: boolean;
+  popular: boolean;
+}
