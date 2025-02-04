ebca56feed1b1d8fc8104fd389fca9123046bebd
david <ticotico6365@gmail.com>
2025-02-04 21:01:51 +0100

    💄 add user details

--- src/app/features/dashboard/pages/user-details-page/user-details-page.component.ts
+++ src/app/features/dashboard/pages/user-details-page/user-details-page.component.ts
@@ -1,25 +1,39 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {ShowUserResponse} from '@models/user.model';
 import {UserService} from '@dashboard/services/user.service';
-import {routes} from '../../../../app.routes';
+import {ActivatedRoute} from '@angular/router';
+import {Card} from 'primeng/card';
+import {DatePipe} from '@angular/common';
+import {TableOrderComponent} from '@dashboard/components/table-order/table-order.component';
 
 @Component({
   selector: 'app-user-details-page',
-  imports: [],
+  imports: [
+    Card,
+    DatePipe,
+    TableOrderComponent
+  ],
   templateUrl: './user-details-page.component.html',
   styles: ``
 })
 export class UserDetailsPageComponent implements OnInit {
 
-  // protected user!: ShowUserResponse;
-  // private userId!: string;
-  // private brawlTag: string = '';
-  //
-  // userService = inject(UserService);
-  // private router: any;
-  //
-  // ngOnInit() {
-  //   this.brawlTag = this.router.url.split('/').pop() || '';
+  protected user!: ShowUserResponse;
+  private userId!: string;
+  private brawlTag: string = '';
+
+  userService = inject(UserService);
+  private router: any;
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.brawlTag = this.route.snapshot.paramMap.get('brawlTag')!;
+
+    this.userService.showUser(this.brawlTag).subscribe((user) => {
+      this.user = user;
+    });
   }
 
 }
