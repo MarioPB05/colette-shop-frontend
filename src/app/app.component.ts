be9f797935e588c08ce6ec28037629be278f2040
Mario <mperdiguerobarrera@safareyes.es>
2025-02-05 17:40:57 +0100

    ✨ Implements toast service and confirmation service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoadingComponent} from '@shared/components/loading/loading.component';
+import {Toast} from "primeng/toast";
+import {ConfirmationService, MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoadingComponent],
+  imports: [RouterOutlet, LoadingComponent, Toast],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [ConfirmationService, MessageService],
 })
 export class AppComponent {
 }
