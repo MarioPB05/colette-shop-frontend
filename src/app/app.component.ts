eaf3fa6679252bb0221f075409ecd84c02a91f04
david <dperezfernandez@safareyes.es>
2025-02-06 12:05:03 +0100

    ✨ Add toast to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoadingComponent} from '@shared/components/loading/loading.component';
+import {ConfirmationService, MessageService} from 'primeng/api';
+import {Toast} from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoadingComponent],
+  imports: [RouterOutlet, LoadingComponent, Toast],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  styleUrl: './app.component.scss',
+  providers: [ConfirmationService, MessageService],
 })
 export class AppComponent {
 }
