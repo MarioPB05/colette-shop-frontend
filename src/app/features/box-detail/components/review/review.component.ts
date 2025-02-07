f3759230a4f538916dd237645a840d10e2fb025b
david <dperezfernandez@safareyes.es>
2025-02-07 20:12:30 +0100

    🚧 Created card review component

---
+++ src/app/features/box-detail/components/review/review.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review',
+  imports: [],
+  templateUrl: './review.component.html',
+  standalone: true,
+  styleUrls: ['./../../../../shared/brawl_styles.scss']
+})
+export class ReviewComponent {
+
+}
