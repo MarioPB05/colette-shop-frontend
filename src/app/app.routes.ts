3a64ebd626aac88790adf5238ca6875a5ff0a6d5
david <dperezfernandez@safareyes.es>
2025-02-07 17:41:48 +0100

    ✨ Add box detail page to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import {CatalogPageComponent} from '@features/catalog/pages/catalog-page/catalog-page.component';
+import {BoxDetailPageComponent} from '@features/box-detail/pages/box-detail-page/box-detail-page.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'shop',
     component: CatalogPageComponent
+  },
+  {
+    path: 'box/:id',
+    component: BoxDetailPageComponent
   }
 ];
