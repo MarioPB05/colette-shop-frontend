6eb14aed2f56656257a44aefaa97fa155cfd2af2
Mario <mperdiguerobarrera@safareyes.es>
2025-02-07 16:19:51 +0100

    ✨ Create core auth service

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {catchError, map, Observable} from 'rxjs';
+import {AuthVerifyResponse} from '@models/auth.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private tokenKey = 'api_token';
+
+  constructor(private http: HttpClient) { }
+
+  setToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  removeToken(): void {
+    localStorage.removeItem(this.tokenKey);
+  }
+
+  isAuthenticated(): Observable<boolean> {
+    return this.http.get<AuthVerifyResponse>('/api/auth/verify').pipe(
+      map((response) => response.valid),
+      catchError(() => {
+        this.removeToken();
+        return [false];
+      })
+    );
+  }
+
+  isAdministrator(): Observable<boolean> {
+    return this.http.get<AuthVerifyResponse>('/api/auth/verify-admin').pipe(
+      map((response) => response.valid),
+      catchError(() => {
+        this.removeToken();
+        return [false];
+      })
+    );
+  }
+
+  isUser(): Observable<boolean> {
+    return this.http.get<AuthVerifyResponse>('/api/auth/verify-user').pipe(
+      map((response) => response.valid),
+      catchError(() => {
+        this.removeToken();
+        return [false];
+      })
+    );
+  }
+}
