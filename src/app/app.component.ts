71bc8d59637228bd97e613e83fc094f547ae2021
Mario <mperdiguerobarrera@safareyes.es>
2025-02-07 16:20:42 +0100

    🔥 Remove providers (Moved to app config file)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoadingComponent} from '@shared/components/loading/loading.component';
 import {Toast} from "primeng/toast";
-import {ConfirmationService, MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, LoadingComponent, Toast],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  providers: [ConfirmationService, MessageService],
   standalone: true,
 })
 export class AppComponent {
