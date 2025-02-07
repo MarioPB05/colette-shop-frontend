7e6383f75f6f02341f9942900aea8b7d2c284743
Mario <mperdiguerobarrera@safareyes.es>
2025-02-07 15:25:14 +0100

    ✨ Fix loading app bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, Renderer2} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoadingComponent} from '@shared/components/loading/loading.component';
 import {ConfirmationService, MessageService} from 'primeng/api';
 import {Toast} from 'primeng/toast';
+import {Observable, Subscription} from 'rxjs';
+import {LoadingService} from '@core/services/loading.service';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +14,23 @@ import {Toast} from 'primeng/toast';
   styleUrl: './app.component.scss',
   providers: [ConfirmationService, MessageService],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  loading$: Observable<boolean>;
+  private subscription!: Subscription;
+
+  constructor(private loadingService: LoadingService, private renderer: Renderer2) {
+    this.loading$ = this.loadingService.loading$;
+    this.subscription = this.loading$.subscribe((isLoading) => {
+      if (isLoading) {
+        this.renderer.addClass(document.body, 'overflow-hidden');
+      } else {
+        this.renderer.removeClass(document.body, 'overflow-hidden');
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    this.renderer.removeClass(document.body, 'overflow-hidden');
+  }
 }
