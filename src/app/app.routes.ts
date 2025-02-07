a87b319e642606d695b4f3ca88c0e75acef49450
Mario <mperdiguerobarrera@safareyes.es>
2025-02-07 16:26:29 +0100

    🔒️ Protect the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,28 @@
 import { Routes } from '@angular/router';
 import {AuthPageComponent} from '@features/auth/pages/auth-page.component';
 import {CatalogPageComponent} from '@features/catalog/pages/catalog-page/catalog-page.component';
+import {authGuard} from '@guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('@dashboard/dashboard.module').then(
       m => m.DashboardModule
-    )
+    ),
+    canActivate: [authGuard],
+    data: { role: 'admin' }
   },
   {
     path: '',
-    component: CatalogPageComponent
+    component: CatalogPageComponent,
+    canActivate: [authGuard],
+    data: { role: 'authenticated' }
   },
   {
     path: 'shop',
-    component: CatalogPageComponent
+    component: CatalogPageComponent,
+    canActivate: [authGuard],
+    data: { role: 'authenticated' }
   },
   {
     path: 'auth',
