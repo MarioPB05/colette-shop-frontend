3142c098a3d7eb953fde9c2755e675c140d75afb
david <dperezfernandez@safareyes.es>
2025-02-10 16:37:28 +0100

    🚧 Created interfaces for box types and images

---
+++ src/app/core/enums/box.enum.ts
@@ -0,0 +1,14 @@
+
+export enum BoxTypeEnum {
+  BOX = 'Caja',
+  BIG_BOX = 'Caja grande',
+  MEGABOX = 'Megacaja',
+  OMEGABOX = 'Omegacaja'
+}
+
+export enum BoxTypeImageEnum {
+  'Caja' = 'common-box.png',
+  'Caja grande' = 'big-box.png',
+  'Megacaja' = 'megabox.png',
+  'Omegacaja' = 'omegabox.png'
+}
