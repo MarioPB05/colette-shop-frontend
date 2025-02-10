c7d9ed8497e70468180c0740765779b803bc0dfb
david <ticotico6365@gmail.com>
2025-02-10 20:53:47 +0100

    ♻️ Add the correction

--- src/app/features/dashboard/components/stat-card/stat-card.component.ts
+++ src/app/features/dashboard/components/stat-card/stat-card.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, HostBinding, HostListener, Input} from '@angular/core';
 import {Card} from 'primeng/card';
+import {NgClass} from '@angular/common';
 
 export interface StatCard {
   title: string;
@@ -7,16 +8,22 @@ export interface StatCard {
   secondaryValue: number;
   icon: string;
   color: string;
+  colorHex: string;
+  rawData: any;
+  key: string;
+  unit: string;
 }
 
 @Component({
   selector: 'app-stat-card',
   imports: [
-    Card
+    Card,
+    NgClass
   ],
   templateUrl: './stat-card.component.html',
   styles: ``
 })
 export class StatCardComponent {
   @Input() statCard!: StatCard;
+  @HostBinding('class') class = 'w-full';
 }
