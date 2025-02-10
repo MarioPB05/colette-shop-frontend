0c8239c0f0655363b901ae3a76f460420553433c
david <dperezfernandez@safareyes.es>
2025-02-10 16:42:42 +0100

    ✨ Create variables and sort brawler's rarities

--- src/app/features/box-detail/pages/box-detail-page/box-detail-page.component.ts
+++ src/app/features/box-detail/pages/box-detail-page/box-detail-page.component.ts
@@ -5,6 +5,10 @@ import {BrawlerProbabilityResponse} from '@models/brawler.model';
 import {NgForOf, NgIf} from '@angular/common';
 import {Tooltip} from 'primeng/tooltip';
 import {ReviewComponent} from '@features/box-detail/components/review/review.component';
+import {CartBtnComponent} from '@shared/components/cart-btn/cart-btn.component';
+import {ListRarityResponse} from '@models/rarity.model';
+import {BoxDetailResponse} from '@models/box.model';
+import {ReviewResponse} from '@models/review.model';
 
 @Component({
   selector: 'app-box-detail-page',
@@ -13,18 +17,28 @@ import {ReviewComponent} from '@features/box-detail/components/review/review.com
     NgForOf,
     NgIf,
     Tooltip,
-    ReviewComponent
+    ReviewComponent,
+    CartBtnComponent
   ],
   templateUrl: './box-detail-page.component.html',
   standalone: true,
   styleUrls: ['./../../../../shared/brawl_styles.scss']
 })
 export class BoxDetailPageComponent implements OnInit {
-  rarities: string[] = ['Inicial', 'Común', 'Raro', 'Super Raro', 'Épico', 'Mítico', 'Legendario'];
+  box: BoxDetailResponse = {
+    id: 1,
+    name: 'Caja de inicio',
+    price: 30,
+    type: 'Normal',
+    boxesLeft: 100,
+    brawlerQuantity: 3
+  }
+  rarities: ListRarityResponse[] = [];
   brawlers: BrawlerProbabilityResponse[] = [
     {
       id: 16000000,
       name: 'Shelly',
+      rarityId: 1,
       rarity: 'Inicial',
       image: '/images/brawlers/16000000_main.png',
       probability: 98,
@@ -33,6 +47,7 @@ export class BoxDetailPageComponent implements OnInit {
     {
       id: 16000001,
       name: 'Colt',
+      rarityId: 2,
       rarity: 'Común',
       image: '/images/brawlers/16000001_main.png',
       probability: 55,
@@ -41,6 +56,7 @@ export class BoxDetailPageComponent implements OnInit {
     {
       id: 16000002,
       name: 'Bull',
+      rarityId: 2,
       rarity: 'Común',
       image: '/images/brawlers/16000002_main.png',
       probability: 30,
@@ -49,6 +65,7 @@ export class BoxDetailPageComponent implements OnInit {
     {
       id: 16000003,
       name: 'Brock',
+      rarityId: 4,
       rarity: 'Super Raro',
       image: '/images/brawlers/16000003_main.png',
       probability: 10,
@@ -57,6 +74,7 @@ export class BoxDetailPageComponent implements OnInit {
     {
       id: 16000004,
       name: 'Rico',
+      rarityId: 3,
       rarity: 'Raro',
       image: '/images/brawlers/16000004_main.png',
       probability: 5,
@@ -65,6 +83,7 @@ export class BoxDetailPageComponent implements OnInit {
     {
       id: 16000005,
       name: 'Spike',
+      rarityId: 6,
       rarity: 'Legendario',
       image: '/images/brawlers/16000005_main.png',
       probability: 2,
@@ -73,6 +92,7 @@ export class BoxDetailPageComponent implements OnInit {
     {
       id: 16000006,
       name: 'Barley',
+      rarityId: 3,
       rarity: 'Raro',
       image: '/images/brawlers/16000006_main.png',
       probability: 1,
@@ -81,12 +101,56 @@ export class BoxDetailPageComponent implements OnInit {
     {
       id: 16000007,
       name: 'Jessie',
+      rarityId: 3,
       rarity: 'Raro',
       image: '/images/brawlers/16000007_main.png',
       probability: 0.5,
       userFavorite: false
     }
   ];
+  reviews: ReviewResponse[] = [
+    {
+      id: 1,
+      idUser: 1,
+      username: 'Javier',
+      rating: 4,
+      comment: 'Muy buen producto, me encantó la calidad y el precio es muy bueno. Lo recomiendo mucho.',
+      post_date: '2025-01-01 12:00:00'
+    },
+    {
+      id: 2,
+      idUser: 2,
+      username: 'Carlos',
+      rating: 5,
+      comment: 'Excelente producto, muy buena calidad y precio. Lo recomiendo mucho.',
+      post_date: '2025-01-01 12:00:00'
+    },
+    {
+      id: 3,
+      idUser: 3,
+      username: 'Andrea',
+      rating: 3,
+      comment: 'Buen producto, la calidad es buena pero el precio es un poco alto. Lo recomiendo mucho.',
+      post_date: '2025-01-01 12:00:00'
+    },
+    {
+      id: 4,
+      idUser: 4,
+      username: 'Luis',
+      rating: 2,
+      comment: 'Malo producto, la calidad es mala y el precio es muy alto. No lo recomiendo mucho.',
+      post_date: '2025-01-01 12:00:00'
+    },
+    {
+      id: 5,
+      idUser: 5,
+      username: 'Sofía',
+      rating: 1,
+      comment: 'Pésimo producto, la calidad es pésima y el precio es muy alto. No lo recomiendo mucho.',
+      post_date: '2025-01-01 12:00:00'
+    }
+  ];
+  averageRating = 0;
 
   brawlersByRarity: {[key: string]: BrawlerProbabilityResponse[]} = {};
 
@@ -95,17 +159,30 @@ export class BoxDetailPageComponent implements OnInit {
 
   ngOnInit() {
     this.faviconService.changeFavicon('/images/favicon/box-favicon.png');
-    this.deleteUnusedRarities();
+    this.getRarities();
     this.putBrawlersByRarity();
+    this.averageRating = this.getAverageRating();
   }
 
-  deleteUnusedRarities() {
-    this.rarities = this.rarities.filter(rarity => this.brawlers.some(brawler => brawler.rarity === rarity));
+  getRarities() {
+    this.rarities = this.brawlers.map(brawler => {
+      return {
+        id: brawler.rarityId,
+        name: brawler.rarity
+      }
+    });
+    this.rarities = this.rarities.filter((rarity, index, self) => self.findIndex(t => t.id === rarity.id) === index);
+    this.rarities.sort((a, b) => a.id - b.id);
   }
 
   putBrawlersByRarity() {
     this.rarities.forEach(rarity => {
-      this.brawlersByRarity[rarity] = this.brawlers.filter(brawler => brawler.rarity === rarity);
+      this.brawlersByRarity[rarity.name] = this.brawlers.filter(brawler => brawler.rarity === rarity.name);
     });
+  }
+
+  getAverageRating() {
+    const sum = this.reviews.reduce((acc, review) => acc + review.rating, 0);
+    return Math.round((sum / this.reviews.length) * 10) / 10;
   }
 }
