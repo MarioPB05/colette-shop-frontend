701b09ae542cb22d2ad453b3bdef7e80fe03da6b
david <ticotico6365@gmail.com>
2025-02-11 17:43:44 +0100

    ♻️ change chart data

--- src/app/features/dashboard/pages/stats-page/stats-page.component.ts
+++ src/app/features/dashboard/pages/stats-page/stats-page.component.ts
@@ -1,8 +1,7 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, ElementRef, inject, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {Tab, TabList, TabPanel, TabPanels, Tabs} from 'primeng/tabs';
 import {StatCard, StatCardComponent} from '@dashboard/components/stat-card/stat-card.component';
 import {StatService} from '@dashboard/services/stat.service';
-import {InventoryStats} from '@models/stats.model';
 import {MessageService} from 'primeng/api';
 import {NgIf} from '@angular/common';
 import {UIChart} from 'primeng/chart';
@@ -27,44 +26,59 @@ export class StatsPageComponent implements OnInit {
   loadingData: boolean = true;
   protected statCards: StatCard[] = [];
 
-  protected data: any;
+  protected dataPedidos: any;
+  protected dataCajas: any;
+  protected dataGemas: any;
   protected options: any;
+  protected stats: any;
 
   private statService = inject(StatService);
   private messageService: MessageService = inject(MessageService);
+
+  // Get all p-chart elements
+  @ViewChildren('.my-chart') charts!: QueryList<ElementRef>;
 
   ngOnInit() {
     this.statService.getStats().subscribe({
       next: (stats) => {
         this.statCards = [{
-          title: 'Cajas',
+          title: 'Cajas compradas',
           principalValue: 0,
           secondaryValue: 0,
+          tertiaryValue: 0,
           icon: 'pi pi-shopping-bag text-blue-500',
+          tabIcon: 'pi pi-shopping-bag',
           color: 'bg-blue-200',
           colorHex: '#60A5FA',
           rawData: stats.inventory,
           key: 'boxes',
+          keyChart: 'bar',
           unit: 'cajas'
         }, {
-          title: 'Precio total',
+          title: 'Ingresos totales',
           principalValue: 0,
           secondaryValue: 0,
+          tertiaryValue: 0,
           icon: 'pi pi-euro text-green-500',
+          tabIcon: 'pi pi-euro',
           color: 'bg-green-200',
           rawData: stats.inventory,
           key: 'totalPrice',
+          keyChart: 'line',
           colorHex: '#34D399',
           unit: '€'
         }, {
-          title: 'Gemas',
+          title: 'Gemas utilizadas',
           principalValue: 0,
           secondaryValue: 0,
+          tertiaryValue: 0,
           icon: 'pi pi-tag text-purple-500',
+          tabIcon: 'pi pi-tag',
           color: 'bg-purple-200',
           colorHex: '#9F7AEA',
           rawData: stats.gems,
           key: 'gems',
+          keyChart: 'line',
           unit: 'gemas'
         }];
 
@@ -73,20 +87,23 @@ export class StatsPageComponent implements OnInit {
         const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
         const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
 
-        this.data = {
-          labels: stats.inventory.map((stat: InventoryStats) => new Date(stat.day).toLocaleDateString()),
-          datasets: []
-        };
+        this.stats = [];
 
         this.statCards.forEach((card) => {
           this.renderStatCard(card);
 
-          this.data.datasets.push({
-            label: card.title,
-            data: card.rawData.map((stat: any) => stat[card.key]),
-            fill: false,
-            borderColor: card.colorHex,
-            tension: 0.4
+          this.stats.push({
+            key: card.key,
+            type: card.keyChart,
+            labels: card.rawData.map((stat: any) => new Date(stat.day).toLocaleDateString()),
+            datasets: [{
+              label: card.title,
+              data: card.rawData.map((stat: any) => stat[card.key]),
+              fill: false,
+              backgroundColor: card.colorHex,
+              borderColor: card.colorHex,
+              tension: 0.4
+            }]
           });
         });
 
@@ -122,6 +139,13 @@ export class StatsPageComponent implements OnInit {
             }
         }
 
+        console.log('Pepe')
+
+        this.charts.forEach((chart) => {
+          console.log(chart);
+          // chart.refresh();
+        });
+
         this.loadingData = false;
       },
       error: () => {
@@ -138,6 +162,8 @@ export class StatsPageComponent implements OnInit {
         statCard.secondaryValue = stat[statCard.key];
       }
     });
+
+    statCard.tertiaryValue = statCard.principalValue / statCard.rawData.length;
   }
 
 }
