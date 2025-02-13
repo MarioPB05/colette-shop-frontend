6dbcc21686cb217ffed972b39a63444892af2821
david <ticotico6365@gmail.com>
2025-02-13 20:26:11 +0100

    ✨ Create the details page

---
+++ src/app/features/dashboard/pages/order-details-page/order-details-page.component.ts
@@ -0,0 +1,67 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {NgIf} from '@angular/common';
+import {OrderService} from '@dashboard/services/order.service';
+import {InventoryOrderDetailsResponse, OrderDetailsResponse} from '@models/order.model';
+import {ActivatedRoute} from '@angular/router';
+import {PrimeTemplate} from 'primeng/api';
+import {TableModule} from 'primeng/table';
+import {BoxTypeImages} from '@core/enums/box.enum';
+
+@Component({
+  selector: 'app-order-details-page',
+  imports: [
+    NgIf,
+    PrimeTemplate,
+    TableModule,
+  ],
+  templateUrl: './order-details-page.component.html',
+  styles: ``
+})
+export class OrderDetailsPageComponent implements OnInit {
+
+  loadingData: boolean = true;
+  protected orderDetails!: OrderDetailsResponse;
+  protected orderDetailsItems: InventoryOrderDetailsResponse[] = [];
+
+  private orderService = inject(OrderService);
+  private route: ActivatedRoute = inject(ActivatedRoute);
+
+  protected readonly BoxTypeImages = BoxTypeImages;
+
+  ngOnInit(): void {
+
+    let orderId = this.route.snapshot.paramMap.get('orderId')!;
+
+    this.orderService.getOrderDetails(orderId).subscribe({
+      next: (order) => {
+        this.orderDetails = order;
+        this.loadingData = false;
+
+        for (let item of order.inventory) {
+          let itemIndex = this.orderDetailsItems.findIndex((i) => i.id === item.id);
+          if (itemIndex !== -1) {
+            this.orderDetailsItems[itemIndex].quantity++;
+          } else {
+            this.orderDetailsItems.push({...item, quantity: 1});
+          }
+        }
+
+        this.orderDetailsItems.forEach((item) => {
+          item.totalPrice = item.price * item.quantity;
+        });
+      },
+      error: (error) => {
+        console.error('Error getting order details:', error);
+        this.loadingData = false;
+      }
+    });
+  }
+
+  subtotal(num:number): number {
+    return Math.round((num/1.21)*100)/100;
+  }
+
+  iva(num:number): number {
+    return Math.round((num*0.21)*100)/100;
+  }
+}
