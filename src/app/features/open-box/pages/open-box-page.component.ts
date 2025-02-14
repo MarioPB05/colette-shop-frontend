affa4a0b35bd8d38ad00f6def5db063ef04a192c
DavidClase <dperezfernandez@safareyes.es>
2025-02-14 21:13:40 +0100

    ✨ Create box open phases and alternate between them

--- src/app/features/open-box/pages/open-box-page.component.ts
+++ src/app/features/open-box/pages/open-box-page.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {NgIf} from '@angular/common';
+import {FaviconService} from '@core/services/favicon.service';
+
+type pageTypes = 'open-box' | 'new-brawler' | 'duplicate-brawler';
 
 @Component({
   selector: 'app-open-box-page',
@@ -13,15 +16,36 @@ import {NgIf} from '@angular/common';
 export class OpenBoxPageComponent implements OnInit{
   pageLoaded = false;
   flashVisible = false;
+  actualPage: pageTypes = 'open-box';
+
+  constructor(private  faviconService: FaviconService) {}
 
   ngOnInit() {
+    this.faviconService.changeFavicon("/images/favicon/box-favicon.png");
     this.pageLoaded = true;
   }
 
-  showFlash() {
-    this.flashVisible = true;
-    setTimeout(() => {
-      this.flashVisible = false;
-    }, 500);
+  showFlash(): Promise<void> {
+    return new Promise<void>((resolve) => {
+      this.flashVisible = true;
+      setTimeout(() => {
+        resolve();
+      }, 300);
+    })
+  }
+
+  openBox() {
+    this.showFlash().then(() => {
+      this.actualPage = 'duplicate-brawler';
+      console.log(this.actualPage);
+    });
+  }
+
+  getParentActualPageClasses() {
+    if (this.actualPage == 'duplicate-brawler') {
+      return 'bg-gradient-radial via-brawl-purple to-brawl-purple from-purple-500';
+    }
+
+    return 'bg-gradient-radial via-brawl-dark-blue to-brawl-dark-blue from-brawl-blue';
   }
 }
