aef023e20aa4977120658f825ebc3dd88593bde3
DavidClase <dperezfernandez@safareyes.es>
2025-02-18 20:59:23 +0100

    ✨ Create user brawler probability interface

--- src/app/core/models/brawler.model.ts
+++ src/app/core/models/brawler.model.ts
@@ -23,3 +23,12 @@ export interface BrawlerProbabilityResponse {
   user_favorite: boolean;
   probability: number;
 }
+
+export interface UserBrawlerProbabilityResponse {
+  id: number;
+  name: string;
+  image: string;
+  model_image: string;
+  probability: number;
+  quantity: number;
+}
