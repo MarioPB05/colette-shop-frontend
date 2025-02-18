3751c54f5f12a3e1ab3d377db3554d3855ab6cae
DavidClase <dperezfernandez@safareyes.es>
2025-02-18 21:09:57 +0100

    🐛 Fix trophy service

--- src/app/shared/services/trophy.service.ts
+++ src/app/shared/services/trophy.service.ts
@@ -8,35 +8,41 @@ export class TrophyService {
   stepTrophies = 25;
 
   getTotalTrophies(brawlerQuantity: number) {
+    // Obtener los trofeos totales basados en la cantidad de brawlers
     brawlerQuantity--;
 
     return brawlerQuantity * this.stepTrophies;
   }
 
   getTrophiesToNextTier(trophies: number) {
-    const tier = this.getTier(trophies) - 1;
+    // Obtener los trofeos necesarios para llegar al siguiente rango
+    const tier = this.getTier(trophies);
 
     if (this.isMaxTier(tier)) {
       return 0;
     }
 
-    if (tier === 0) {
-      return 0;
-    }
-
-    return trophies - this.getTrophyTierCount(tier + 1);
+    return this.getTrophyTierCount(tier) - trophies;
   }
 
   getTier(trophies: number) {
-    return TrophiesToGetTier.reduce((maxIndex, total, index) =>
-      total <= trophies ? index : maxIndex, 0) + 1;
+    // Obtener el rango en el que se encuentra el usuario según sus trofeos
+    let tier = 1;
+
+    while (trophies >= this.getTrophyTierCount(tier)) {
+      tier++;
+    }
+
+    return tier;
   }
 
   getTrophyTierCount(tier: number) {
+    // Obtener el total de trofeos necesarios para llegar a un rango
     return TrophiesToGetTier[tier - 1];
   }
 
   getTrophiesToReachTier(tier: number) {
+    // Obtener los trofeos necesarios para subir a ese rango
     return TrophiesToReachTier[tier - 1];
   }
 
