cc9c26a6d7e685df363bf08bfc74b2efcdea0598
DavidClase <dperezfernandez@safareyes.es>
2025-02-21 21:07:09 +0100

    ✨ Add rarity detail interface

--- src/app/core/models/rarity.model.ts
+++ src/app/core/models/rarity.model.ts
@@ -3,3 +3,11 @@ export interface ListRarityResponse {
   id: number;
   name: string;
 }
+
+export interface RarityDetailResponse {
+  id: number;
+  name: string;
+  color: string;
+  brawlersOfRarityUnlocked: number;
+  totalBrawlersOfRarity: number;
+}
