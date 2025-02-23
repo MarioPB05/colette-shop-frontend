b476d812d9667ed50513515fd3641c55a692f483
Mario <mperdiguerobarrera@safareyes.es>
2025-02-23 12:47:41 +0100

    🐛 Fix loading bug

--- src/app/core/services/loading.service.ts
+++ src/app/core/services/loading.service.ts
@@ -8,12 +8,19 @@ export class LoadingService {
   private loadingSubject = new BehaviorSubject<boolean>(false);
 
   loading$ = this.loadingSubject.asObservable();
+  count = 0;
 
   loadingOn() {
+    this.count++;
     this.loadingSubject.next(true);
   }
 
   loadingOff() {
+    if (this.count > 1) {
+      this.count--;
+      return;
+    }
+
     this.loadingSubject.next(false);
   }
 }
