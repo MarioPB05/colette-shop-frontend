4aa8b8a70e0b31d2b64e905284ba4e32f0604c4f
Mario <mperdiguerobarrera@safareyes.es>
2025-02-23 12:47:07 +0100

    ♻️ Add skip loading context

--- src/app/features/box-detail/services/review.service.ts
+++ src/app/features/box-detail/services/review.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpContext} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {ReviewResponse} from '@models/review.model';
+import {SkipLoading} from '@interceptors/loading.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,8 @@ export class ReviewService {
   constructor(private http: HttpClient) {}
 
   getReviewsFromBox(id: number): Observable<ReviewResponse[]> {
-    return this.http.get<ReviewResponse[]>(`${this.apiUrl}/box/${id}`);
+    return this.http.get<ReviewResponse[]>(`${this.apiUrl}/box/${id}`, {
+      context: new HttpContext().set(SkipLoading, true)
+    });
   }
 }
