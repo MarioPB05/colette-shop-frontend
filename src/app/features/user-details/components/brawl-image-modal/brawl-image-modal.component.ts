81e4674e14bbcc2b8451410ad204387b96ac8364
david <ticotico6365@gmail.com>
2025-02-24 14:57:03 +0100

    💄 change for real style

--- src/app/features/user-details/components/brawl-image-modal/brawl-image-modal.component.ts
+++ src/app/features/user-details/components/brawl-image-modal/brawl-image-modal.component.ts
@@ -1,21 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FloatLabel} from 'primeng/floatlabel';
 import {InputText} from 'primeng/inputtext';
 import {NgIf} from '@angular/common';
+import {Select} from 'primeng/select';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-brawl-image-modal',
   imports: [
-    FloatLabel,
-    InputText,
-    NgIf
+    NgIf,
+    Select,
+    FormsModule
   ],
   templateUrl: './brawl-image-modal.component.html',
   styleUrls: ['./../../../../shared/brawl_styles.scss']
 })
-export class BrawlImageModalComponent {
+export class BrawlImageModalComponent implements OnInit {
 
   isOpen = false;
+
+  brawlers: any[] | undefined;
+
+  selectedBrawler: string | undefined;
+
+  ngOnInit() {
+    this.brawlers = [
+      { name: 'Australia', code: 'AU' },
+      { name: 'Brazil', code: 'BR' },
+      { name: 'China', code: 'CN' },
+      { name: 'Egypt', code: 'EG' },
+      { name: 'France', code: 'FR' },
+      { name: 'Germany', code: 'DE' },
+      { name: 'India', code: 'IN' },
+      { name: 'Japan', code: 'JP' },
+      { name: 'Spain', code: 'ES' },
+      { name: 'United States', code: 'US' }
+    ];
+  }
 
   openModal() {
     this.isOpen = true;
