d894a07011903391e3f386fc973d0c984e93f022
david <ticotico6365@gmail.com>
2025-02-28 21:07:44 +0100

    ♻️ refactor methods

--- src/app/features/user-details/components/brawl-image-modal/brawl-image-modal.component.ts
+++ src/app/features/user-details/components/brawl-image-modal/brawl-image-modal.component.ts
@@ -1,10 +1,13 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, inject, Input, OnInit, Output} from '@angular/core';
 import {FloatLabel} from 'primeng/floatlabel';
 import {InputText} from 'primeng/inputtext';
 import {NgIf} from '@angular/common';
 import {Select} from 'primeng/select';
 import {FormsModule} from '@angular/forms';
-import {BrawlerUserDetailsResponse} from '@models/user.model';
+import {BrawlerUserDetailsResponse, UserBrawler} from '@models/user.model';
+import {MessageService} from 'primeng/api';
+import {UserService} from '@features/user-details/service/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-brawl-image-modal',
@@ -20,21 +23,47 @@ export class BrawlImageModalComponent implements OnInit {
 
   isOpen = false;
 
-  @Input() brawlers!: BrawlerUserDetailsResponse[];
-  @Input() userBrawler!: BrawlerUserDetailsResponse;
+  @Input() brawlers!: UserBrawler[];
+  @Input() userBrawler!: UserBrawler;
+  @Output() userBrawlerChange: EventEmitter<UserBrawler> = new EventEmitter<UserBrawler>();
 
-  selectedBrawler: BrawlerUserDetailsResponse | undefined;
+  protected selectedBrawler!: UserBrawler;
+  private messageService: MessageService = inject(MessageService);
+  private userService: UserService = inject(UserService);
 
   ngOnInit() {
     this.selectedBrawler = this.userBrawler;
   }
 
   openModal() {
-    this.isOpen = true;
+    if (this.brawlers.length > 0) {
+      this.selectedBrawler = this.userBrawler;
+      this.isOpen = true;
+    } else {
+      this.messageService.add({severity: 'error', summary: 'Error', detail: 'No tienes brawlers'});
+      this.isOpen = false;
+    }
   }
 
   closeModal() {
     this.isOpen = false;
+  }
+
+  saveBrawler() {
+    console.log(this.selectedBrawler);
+    this.userService.setBrawlerImage(this.selectedBrawler.id).subscribe({
+      next: () => {
+        this.isOpen = false;
+        this.userBrawlerChange.emit(this.selectedBrawler);
+      },
+      error: (err) => {
+        this.messageService.add({severity: 'error', summary: 'Error', detail: 'No se ha podido guardar la imagen'});
+        console.error(err);
+        console.log(this.selectedBrawler);
+      }
+    });
+
+
   }
 
 }
