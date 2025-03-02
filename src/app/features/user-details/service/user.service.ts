e105762959f512f6e94803533729b5ba5b80add8
david <ticotico6365@gmail.com>
2025-03-02 12:56:22 +0100

    🔥 remove innecesary code

--- src/app/features/user-details/service/user.service.ts
+++ src/app/features/user-details/service/user.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpContext} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {BrawlerUserDetailsResponse, UserBrawler, UserChangeRequest, UserDetailsResponse} from '@models/user.model';
+import {UserBrawler, UserChangeRequest, UserDetailsResponse} from '@models/user.model';
 import {OrderUserDetailsResponse} from '@models/order.model';
 import {environment} from '@environments/environment';
+import {SkipLoading} from '@interceptors/loading.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -15,23 +16,27 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getUserDetails(): Observable<UserDetailsResponse> {
-    return this.http.get<UserDetailsResponse>(`${this.apiUrl}/details`);
+    return this.http.get<UserDetailsResponse>(`${environment.baseUrl}/user/details`);
   }
 
   getBrawlersOfUser(): Observable<UserBrawler[]> {
-    return this.http.get<UserBrawler[]>(`${this.apiUrl}/details/brawlers`);
+    return this.http.get<UserBrawler[]>(`${environment.baseUrl}/user/details/brawlers`);
   }
 
   getOrderOfUser(): Observable<OrderUserDetailsResponse[]> {
-    return this.http.get<any>(`${this.apiUrl}/details/orders`);
+    return this.http.get<any>(`${environment.baseUrl}/user/details/orders`);
   }
 
   setBrawlerImage(id: number): Observable<any> {
     console.log(id);
-    return this.http.post(`${environment}/details/user_avatar/${id}`, {});
+    return this.http.post(`${environment.baseUrl}/user/details/brawler_image/${id}`, {}, {
+      context: new HttpContext().set(SkipLoading, true)
+    });
   }
 
   setUserChanges(user: UserChangeRequest): Observable<UserChangeRequest> {
-    return this.http.put<UserChangeRequest>(`${environment.baseUrl}/user/details/user/change_details`, user);
+    return this.http.put<UserChangeRequest>(`${environment.baseUrl}/user/details/change_details`, user, {
+      context: new HttpContext().set(SkipLoading, true)
+    });
   }
 }
