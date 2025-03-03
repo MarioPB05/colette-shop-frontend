36b1da60535a854818a4df85ab46dedb1c6434ef
david <dperezfernandez@safareyes.es>
2025-03-03 12:32:50 +0100

    ✨ Create review box modal

---
+++ src/app/features/open-box/components/add-review-modal/add-review-modal.component.ts
@@ -0,0 +1,87 @@
+import {Component, Input} from '@angular/core';
+import {Rating} from 'primeng/rating';
+import {FormsModule} from '@angular/forms';
+import {Textarea} from 'primeng/textarea';
+import {ReviewService} from '@features/open-box/services/review.service';
+import {Router} from '@angular/router';
+import {MessageService} from 'primeng/api';
+import {NgClass, NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-add-review-modal',
+  imports: [
+    Rating,
+    FormsModule,
+    Textarea,
+    NgIf,
+    NgClass
+  ],
+  templateUrl: './add-review-modal.component.html',
+  standalone: true,
+  styleUrl: './../../../../shared/brawl_styles.scss',
+})
+export class AddReviewModalComponent {
+  rating: number = 0;
+  comment: string = '';
+  userAddedReview: boolean = false;
+  @Input() boxId: number = 1;
+
+  // VALIDATION
+  ratingValid: boolean = true;
+  commentValid: boolean = true;
+
+  constructor(private reviewService: ReviewService,
+              private router: Router,
+              private messageService: MessageService) {}
+
+  isRatingValid(): boolean {
+    return this.rating > 0;
+  }
+
+  isCommentValid(): boolean {
+    return this.comment.length > 0;
+  }
+
+  validateForm(): void {
+    this.ratingValid = this.isRatingValid();
+    this.commentValid = this.isCommentValid();
+  }
+
+  isFormValid(): boolean {
+    return this.ratingValid && this.commentValid;
+  }
+
+  addReview(): void {
+    this.validateForm();
+
+    if (!this.isFormValid()) {
+      return;
+    }
+
+    this.saveReview();
+  }
+
+  saveReview() {
+    this.reviewService.addReview(this.boxId, this.rating, this.comment).subscribe({
+      next: (response) => {
+        if (response.status === 'error') {
+          this.throwReviewError();
+          return;
+        }
+
+        this.userAddedReview = true;
+      },
+      error: (err) => {
+        this.throwReviewError();
+      }
+    });
+  }
+
+  throwReviewError(): void {
+    this.messageService.add({severity: 'error', summary: 'Error', detail: 'No se ha podido añadir la reseña'});
+  }
+
+  goToBoxResume(): void {
+    this.router.navigate([`/box/${this.boxId}/resume`]);
+  }
+}
