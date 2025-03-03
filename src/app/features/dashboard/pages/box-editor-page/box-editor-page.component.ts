dae03a32b22e87e70408e83f47852ad011ecad5e
DavidClase <dperezfernandez@safareyes.es>
2025-03-03 21:20:28 +0100

    ✨ Create reactive form

--- src/app/features/dashboard/pages/box-editor-page/box-editor-page.component.ts
+++ src/app/features/dashboard/pages/box-editor-page/box-editor-page.component.ts
@@ -4,13 +4,15 @@ import {InputText} from 'primeng/inputtext';
 import {InputNumber} from 'primeng/inputnumber';
 import {ListBrawlerResponse} from '@models/brawler.model';
 import {BrawlerService} from '@dashboard/services/brawler.service';
-import {MessageService} from 'primeng/api';
+import {MessageService, PrimeTemplate} from 'primeng/api';
 import {NgClass, NgForOf, NgIf} from '@angular/common';
 import {Tooltip} from 'primeng/tooltip';
-import {FormsModule} from '@angular/forms';
+import {FormControl, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {ToggleSwitch} from 'primeng/toggleswitch';
 import {BoxTypeImages, BoxTypes} from '@core/enums/box.enum';
 import {Select} from 'primeng/select';
+import {CreateBoxRequest, CreateDailyBoxRequest} from '@models/box.model';
+import {BoxService} from '@dashboard/services/box.service';
 
 interface ListBrawler extends ListBrawlerResponse {
   probability: number;
@@ -28,7 +30,9 @@ interface ListBrawler extends ListBrawlerResponse {
     ToggleSwitch,
     Select,
     NgIf,
-    NgClass
+    NgClass,
+    ReactiveFormsModule,
+    PrimeTemplate
   ],
   templateUrl: './box-editor-page.component.html',
   styleUrl: '../../../../shared/brawl_styles.scss'
@@ -39,13 +43,17 @@ export class BoxEditorPageComponent {
 
   boxId!: number;
   editMode = false;
-  previousDefaultProbability = 0;
+  previousDefaultProbability = 0; // Se usa para actualizar la probabilidad de los brawlers
+  isDailyBox = false;
 
-  name = '';
-  price = 0;
-  type = BoxTypes[0];
-  quantity = 0;
-  brawler_quantity = 0;
+  formGroup: FormGroup = new FormGroup({
+    name: new FormControl('', {validators: [Validators.required, Validators.minLength(3)]}),
+    price: new FormControl(0, {validators: [Validators.required, Validators.min(0)]}),
+    type: new FormControl(BoxTypes[0], {validators: [Validators.required]}),
+    quantity: new FormControl(0, {validators: [Validators.required, Validators.min(0)]}),
+    brawler_quantity: new FormControl(0, {validators: [Validators.required, Validators.min(0)]}),
+    repeat_hours: new FormControl(24, {validators: [Validators.required, Validators.min(1)]})
+  });
 
 
   // Variables para la configuración de la caja
@@ -75,8 +83,8 @@ export class BoxEditorPageComponent {
   rarityDefaultProbabilities : {[key: string]: number} = {
     'Inicial': 100,
     'Raro': 70,
-    'Super Raro': 40,
-    'Épico': 20,
+    'Super Raro': 50,
+    'Épico': 30,
     'Mítico': 10,
     'Legendario': 5
   }
@@ -85,7 +93,8 @@ export class BoxEditorPageComponent {
     private router: Router,
     private route: ActivatedRoute,
     private brawlerService: BrawlerService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private boxService: BoxService
   ) {
     this.editMode = this.router.url.includes('edit') && this.route.snapshot.paramMap.has('id');
     this.boxId = Number(this.route.snapshot.paramMap.get('id'));
@@ -100,11 +109,67 @@ export class BoxEditorPageComponent {
     });
   }
 
+  getBoxName(): string {
+    return this.formGroup.get('name')?.value.trim();
+  }
+
+  getBoxType(): string {
+    return this.formGroup.get('type')?.value;
+  }
+
+  getBoxTypeImage(): string {
+    return this.BoxTypeImages[this.getBoxType()];
+  }
+
+  getBoxQuantity(): number {
+    return this.formGroup.get('quantity')?.value;
+  }
+
+  getSendButtonText(): string {
+    return this.editMode ? 'Guardar cambios' : 'Crear caja';
+  }
+
+  getDailyBoxButtonText(): string {
+    return !this.isDailyBox ? 'Caja recurrente' : 'Caja normal';
+  }
+
+  formFieldHasError(field: string): boolean {
+    const control = this.formGroup?.get(field);
+    if (!control) {
+      return false;
+    }
+
+    return control.invalid && control.touched;
+  }
+
+  changeFormToDailyBox(): void {
+    this.formGroup.get('price')?.setValue(0);
+    this.formGroup.get('price')?.disable();
+    this.formGroup.get('quantity')?.setValue(-1);
+    this.formGroup.get('quantity')?.disable();
+  }
+
+  changeFormToNormalBox(): void {
+    this.formGroup.get('price')?.enable();
+    this.formGroup.get('quantity')?.enable();
+    this.formGroup.get('quantity')?.setValue(0);
+  }
+
+  toggleDailyBox(): void {
+    this.isDailyBox = !this.isDailyBox;
+    this.isDailyBox ? this.changeFormToDailyBox() : this.changeFormToNormalBox();
+
+    // scroll smoothly to the top of the page
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+
   toggleUnlimitedQuantity(): void {
-    if (this.quantity === -1) {
-      this.quantity = 0;
+    if (this.formGroup.get('quantity')?.disabled) {
+      this.formGroup.get('quantity')?.setValue(0);
+      this.formGroup.get('quantity')?.enable();
     } else {
-      this.quantity = -1;
+      this.formGroup.get('quantity')?.setValue(-1);
+      this.formGroup.get('quantity')?.disable();
     }
   }
 
@@ -192,5 +257,61 @@ export class BoxEditorPageComponent {
     });
 
     this.storePreviousValue(rarity);
+  }
+
+  convertFormToBoxRequest(): CreateBoxRequest {
+    return {
+      name: this.getBoxName(),
+      price: this.formGroup.get('price')?.value,
+      type: this.getBoxType(),
+      quantity: this.formGroup.get('quantity')?.value,
+      brawler_quantity: this.formGroup.get('brawler_quantity')?.value,
+      brawler_ids: this.selectedBrawlers
+    }
+  }
+
+  convertFormToDailyBoxRequest(): CreateDailyBoxRequest {
+    return {
+      name: this.getBoxName(),
+      type: this.getBoxType(),
+      repeat_every_hours: this.formGroup.get('repeat_hours')?.value,
+      brawler_quantity: this.formGroup.get('brawler_quantity')?.value,
+      brawler_ids: this.selectedBrawlers
+    }
+  }
+
+  saveBox(): void {
+    this.formGroup.markAllAsTouched();
+    this.formGroup.updateValueAndValidity();
+
+    if (this.formGroup.invalid) {
+      return;
+    }
+
+    if (this.isDailyBox) {
+      this.createDailyBox();
+    } else {
+      this.createNormalBox();
+    }
+  }
+
+  createNormalBox(): void {
+    this.boxService.createBox(this.convertFormToBoxRequest()).subscribe({
+      next: () => {
+        this.messageService.add({severity: 'success', summary: 'Éxito', detail: 'Caja creada correctamente'});
+        this.router.navigate(['/dashboard/boxes']);
+      },
+      error: () => this.messageService.add({severity: 'error', summary: 'Error', detail: 'No se pudo crear la caja'})
+    });
+  }
+
+  createDailyBox(): void {
+    this.boxService.createDailyBox(this.convertFormToDailyBoxRequest()).subscribe({
+      next: () => {
+        this.messageService.add({severity: 'success', summary: 'Éxito', detail: 'Caja creada correctamente'});
+        this.router.navigate(['/dashboard/boxes']);
+      },
+      error: () => this.messageService.add({severity: 'error', summary: 'Error', detail: 'No se pudo crear la caja'})
+    });
   }
 }
