d47114cb88c1385b3755f3439b5cfd6338dd45c6
david <ticotico6365@gmail.com>
2025-03-03 12:12:01 +0100

    ✨ Create inventory.component

---
+++ src/app/features/inventory/pages/inventory/inventory.component.ts
@@ -0,0 +1,41 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {BrawlHeaderComponent} from '@shared/components/brawl-header/brawl-header.component';
+import {CloseBoxComponent} from '@features/inventory/components/close-box/close-box.component';
+import {OpenBoxComponent} from '@features/inventory/components/open-box/open-box.component';
+import {InventoryService} from '@features/inventory/service/inventory.service';
+import {InventoryModel} from '@models/inventory.model';
+import {NgForOf, NgIf} from '@angular/common';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-inventory',
+  imports: [
+    BrawlHeaderComponent,
+    CloseBoxComponent,
+    OpenBoxComponent,
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./../../../../shared/brawl_styles.scss']
+})
+export class InventoryComponent implements OnInit {
+
+  protected inventories!: InventoryModel[];
+  protected openBoxes!: InventoryModel[];
+  protected closedBoxes!: InventoryModel[];
+
+  private inventoryService: InventoryService = inject(InventoryService);
+
+  ngOnInit() {
+    this.inventoryService.getInventory().subscribe(
+      inventory => {
+        this.inventories = inventory;
+        this.openBoxes = this.inventories
+          .filter(inventory => inventory.open && inventory.openDate)
+          .sort((a, b) => new Date(b.openDate!).getTime() - new Date(a.openDate!).getTime());
+        this.closedBoxes = this.inventories.filter(inventory => !inventory.open).sort((a, b) => new Date(b.collectDate).getTime() - new Date(a.collectDate).getTime());
+      }
+    );
+  }
+}
