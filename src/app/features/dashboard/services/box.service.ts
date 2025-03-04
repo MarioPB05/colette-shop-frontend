0207916328f9c0f53b84a3f07b9e432b775bbdc0
david <dperezfernandez@safareyes.es>
2025-03-04 13:03:32 +0100

    ♻️ Add skip loading and change routes

--- src/app/features/dashboard/services/box.service.ts
+++ src/app/features/dashboard/services/box.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {TableBoxResponse} from '@core/models/box.model';
+import {HttpClient, HttpContext} from '@angular/common/http';
+import {CreateBoxRequest, CreateDailyBoxRequest, TableBoxResponse} from '@core/models/box.model';
 import {Observable} from 'rxjs';
 import {environment} from '@environments/environment';
+import {SkipLoading} from '@interceptors/loading.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -19,11 +20,15 @@ export class BoxService {
     return this.http.delete<void>(`${environment.baseUrl}/boxes/${id}`)
   }
 
-  createBox(data: any): Observable<void> {
-    return this.http.post<void>(`${environment.baseUrl}/boxes`, data)
+  createBox(data: CreateBoxRequest): Observable<any> {
+    return this.http.post<any>(`${environment.baseUrl}/boxes/`, data, {
+      context: new HttpContext().set(SkipLoading, true)
+    });
   }
 
-  createDailyBox(data: any): Observable<void> {
-    return this.http.post<void>(`${environment.baseUrl}/boxes/daily`, data)
+  createDailyBox(data: CreateDailyBoxRequest): Observable<any> {
+    return this.http.post<any>(`${environment.baseUrl}/boxes/daily`, data, {
+      context: new HttpContext().set(SkipLoading, true)
+    })
   }
 }
